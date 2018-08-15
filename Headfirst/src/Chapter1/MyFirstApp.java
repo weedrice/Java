f844ff153a23e295ce189283365d312bfa579b2a
weedrice <supp0rtyoo@gmail.com>
2018-08-16 00:02:49 +0900

    Chapter 1

--- Headfirst/src/Chapter1/MyFirstApp.java
+++ Headfirst/src/Chapter1/MyFirstApp.java
@@ -1,3 +1,4 @@
+package Chapter1;
 import java.awt.*;
 import java.awt.event.*;
 
---
+++ Headfirst/src/Chapter1/Test.java
@@ -0,0 +1,19 @@
+package Chapter1;
+
+public class Test {
+	public static void main(String[] Args) {
+		int x = 0;
+		int y = 0;
+		while(x < 5)  {
+			if(y < 5) {
+				x = x + 1;
+				if(y < 3) {
+					x = x - 1;
+				}
+				y = y + 2;
+			}
+			System.out.println(x + "" + y + " ");
+			x = x + 1;
+		}
+	}
+}
