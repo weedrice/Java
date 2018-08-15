920a3d20c40fa28cc6e38b9edc543ce27ff4d188
weedrice <supp0rtyoo@gmail.com>
2018-08-15 23:26:28 +0900

    First

---
+++ Headfirst/src/MyFirstApp.java
@@ -0,0 +1,11 @@
+import java.awt.*;
+import java.awt.event.*;
+
+class MyFirstApp {
+
+	public static void main(String[] args) {
+		System.out.println("I Rule!");
+		System.out.println("The World");
+	}
+
+}
