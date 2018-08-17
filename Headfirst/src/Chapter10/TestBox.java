e3dac1f8334cfb680fd0670d06fdb8cb6aaf0d6f
weedrice <supp0rtyoo@gmail.com>
2018-08-17 23:30:37 +0900

    Chapter 11

---
+++ Headfirst/src/Chapter10/TestBox.java
@@ -0,0 +1,17 @@
+package Chapter10;
+
+public class TestBox {
+	Integer i;
+    int j;
+    
+    public static void main(String[] args) {
+        TestBox t = new TestBox();
+        t.go();
+    }
+    
+    public void go() {
+        j=i;
+        System.out.println(j);
+        System.out.println(i);
+    }
+}
